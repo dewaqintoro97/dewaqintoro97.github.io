951617ebad30fb5637c10b74efd027458ca7ccfb
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-05-31 01:58:45 +0700

    v1

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dewaqintoro</title>
+</head>
+<body>
+    <h5>Under Maintenance</h5>
+</body>
+</html>
